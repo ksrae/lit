8e226f2214a5edc23c37221f1d0ebab575a178e5
김성래 <ksrae>
2024-03-20 19:20:14 +0900

    update vite.config

--- branch/vite.config.js
+++ branch/vite.config.js
@@ -1,28 +1,10 @@
-// // vite.config.js basic
-// import { defineConfig } from 'vite'
-// import path from 'path'
-
-// export default defineConfig({
-//   build: {
-//     outDir: 'dist',
-//     lib: {
-//       entry: [
-//         path.resolve(__dirname, 'src/web-component/hello-world.ts'),
-//         path.resolve(__dirname, 'src/web-component/simple-greeting.ts'),
-//       ],
-//       name: 'HelloWorldComponent',
-//       fileName: (format) => `hello-world.${format}.js`
-//     }
-//   }
-// })
-
 // vite.config.js multiple files to one
 import { defineConfig } from 'vite'
 import fs from 'fs'
 import path from 'path'
 
 const srcDir = path.resolve(__dirname, 'src')
-const componentDir = 'web-component'
+const componentDir = 'components'
 
 fs.readdir(`${srcDir}/${componentDir}`, (err, files) => {
   if (err) {
@@ -45,7 +27,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'), // 빌드의 엔트리 포인트를 main.ts로 설정
       name: 'MyWebComponents',
-      fileName: (format) => `my-web-components.${format}.js`,
+      fileName: (format) => `my-components.${format}.js`,
     },
     rollupOptions: {
       // 필요에 따라 설정을 추가하거나 수정합니다.
