2acf433b43517adbab61122d77ecf955181fd2e5
김성래 <ksrae>
2024-03-20 19:27:27 +0900

    fix config

--- branch/vite.config.js
+++ branch/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js multiple files to one
 import { defineConfig } from 'vite'
 import fs from 'fs'
 import path from 'path'
@@ -6,30 +5,38 @@ import path from 'path'
 const srcDir = path.resolve(__dirname, 'src')
 const componentDir = 'components'
 
-fs.readdir(`${srcDir}/${componentDir}`, (err, files) => {
-  if (err) {
-    console.error(err)
-    return
-  }
 
-  const tsFiles = files.filter((file) => file.endsWith('.ts'))
-  const imports = tsFiles.map((file) => `import './${componentDir}/${file}'`).join('\n')
+let allImports = ''; // 모든 import 문을 저장할 변수
 
-  fs.writeFile(path.resolve(srcDir, 'main.ts'), imports, (err) => {
-    if (err) {
-      console.error(err)
+function addImportsFromDirectory(startPath, relativePath = '') {
+  const entries = fs.readdirSync(startPath, {withFileTypes: true});
+
+  entries.forEach(entry => {
+    if (entry.isDirectory()) {
+      addImportsFromDirectory(path.join(startPath, entry.name), path.join(relativePath, entry.name)); // 하위 폴더 탐색
+    } else if (entry.name.endsWith('.ts')) {
+      allImports += `import './${path.join(relativePath, entry.name).replace(/\\/g, '/')}'\n`; // 윈도우에서 경로 구분자 처리하기 위해 replace 사용
     }
-  })
-})
+  });
+}
+
+// 초기 디렉토리 설정 후 재귀 함수 호출
+addImportsFromDirectory(path.join(srcDir, componentDir), componentDir);
+
+// 모든 import 문이 추가된 후에 파일에 한 번에 쓴다.
+fs.writeFileSync(path.resolve(srcDir, 'main.ts'), allImports);
+console.log(`Files written to ${srcDir}/main.ts`);
+
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'), // 빌드의 엔트리 포인트를 main.ts로 설정
-      name: 'MyWebComponents',
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: 'MyComponents',
       fileName: (format) => `my-components.${format}.js`,
     },
     rollupOptions: {
+      
       // 필요에 따라 설정을 추가하거나 수정합니다.
     },
   },
