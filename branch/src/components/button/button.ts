c1cd1d9c2973d52ee8219525a9ebdd7414a0b0bb
sungrae <sungrae@synopsys.com>
2024-03-25 11:30:13 +0900

    no message

--- branch/src/components/button/button.ts
+++ branch/src/components/button/button.ts
@@ -1,9 +1,21 @@
-import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
 @customElement('button-sample')
-class TranslateElement extends LitElement {
+class ButtonElement extends LitElement {
+  static styles = css`
+    button {
+      padding: 8px 16px;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  `;
+
+  @property() value!: string;
+
+
   render() {
-    return html`<button>Sample</button>`;
+    return html`
+    <button>${this.value}</button>`;
   }
 }
